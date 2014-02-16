46c2af957e24f37118e162a89794a53bcc073669
dtr <dtr.blogger@gmail.com>
2014-02-16 07:35:52 -0700

    gitignore update

--- src/main/java/com/dtr/oas/config/Initializer.java
+++ src/main/java/com/dtr/oas/config/Initializer.java
@@ -4,6 +4,8 @@ import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.dtr.oas.config.DatabaseConfig;
+import com.dtr.oas.config.ThymeleafConfig;
+import com.dtr.oas.config.WebAppConfig;
 
 @Order(1)
 public class Initializer extends
@@ -11,12 +13,12 @@ public class Initializer extends
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class[] {DatabaseConfig.class };
+		return new Class[] { DatabaseConfig.class };
 	}
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		return new Class<?>[] { WebAppConfig.class };
+		return new Class<?>[] { ThymeleafConfig.class, WebAppConfig.class };
 	}
 
 	@Override
--- src/main/java/com/dtr/oas/config/WebAppConfig.java
+++ src/main/java/com/dtr/oas/config/WebAppConfig.java
@@ -18,7 +18,7 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
 	@Bean
 	public UrlBasedViewResolver setupViewResolver() {
 		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
-		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setPrefix("/WEB-INF/pages/");
 		resolver.setSuffix(".jsp");
 		resolver.setViewClass(JstlView.class);
 		return resolver;
